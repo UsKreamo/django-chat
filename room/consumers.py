df54449f6dde6fe9749cee9f7e4f5552583b6bf7
UsKreamo <uskreamo@gmail.com>
2022-01-16 13:41:39 +0900

    チャット機能非同期

--- room/consumers.py
+++ room/consumers.py
@@ -1,20 +1,18 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
 
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(WebsocketConsumer):
-  def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+  async def connect(self):
     self.strGroupName = 'chat'
-    async_to_sync(self.channel_layer.group_add)(self.strGroupName, self.channel_name)
+    await self.channel_layer.group_add(self.strGroupName, self.channel_name)
 
-    # pprint.pprint(self.accept())
-    self.accept()
+    await self.accept()
   
-  def disconnect(self, close_code):
-    async_to_sync(self.channel_layer.group_discard)(self.strGroupName, self.channel_name)
+  async def disconnect(self, close_code):
+    await self.channel_layer.group_discard(self.strGroupName, self.channel_name)
 
-  def receive(self, text_data):
+  async def receive(self, text_data):
     text_data_json = json.loads(text_data)
 
     strMessage = text_data_json['message']
@@ -23,11 +21,11 @@ class ChatConsumer(WebsocketConsumer):
       'type': 'chat_message',
       'message': strMessage,
     }
-    async_to_sync(self.channel_layer.group_send)(self.strGroupName, data)
+    await self.channel_layer.group_send(self.strGroupName, data)
 
-  def chat_message(self, data):
+  async def chat_message(self, data):
     data_json = {
       'message': data['message'],
     }
 
-    self.send(text_data=json.dumps(data_json))
+    await self.send(text_data=json.dumps(data_json))
