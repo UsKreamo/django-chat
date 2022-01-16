005f9adb79b19f24267164f3d9f41a628891d2d3
UsKreamo <uskreamo@gmail.com>
2022-01-16 15:17:07 +0900

    ルーム入退室、名前入力機能実装

--- room/consumers.py
+++ room/consumers.py
@@ -1,31 +1,62 @@
 import json
+import datetime
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
-  async def connect(self):
-    self.strGroupName = 'chat'
-    await self.channel_layer.group_add(self.strGroupName, self.channel_name)
 
+  def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.strGroupName = ''
+      self.strUserName = ''
+
+  async def connect(self):
     await self.accept()
   
   async def disconnect(self, close_code):
-    await self.channel_layer.group_discard(self.strGroupName, self.channel_name)
+    await self.leave_chat()
 
   async def receive(self, text_data):
     text_data_json = json.loads(text_data)
 
-    strMessage = text_data_json['message']
+    # 入室時処理
+    if('join' == text_data_json.get('data_type')):
+      self.strUserName = text_data_json["username"]
+      await self.join_chat()
+    # 退室時処理
+    elif('leave' == text_data_json.get('data_type')):
+      await self.leave_chat()
+    # メッセージ受信時処理
+    else:
+      strMessage = text_data_json['message']
 
-    data = {
-      'type': 'chat_message',
-      'message': strMessage,
-    }
-    await self.channel_layer.group_send(self.strGroupName, data)
+      data = {
+        'type': 'chat_message',
+        'message': strMessage,
+        'username': self.strUserName,
+        'datetime': datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'),
+      }
+      await self.channel_layer.group_send(self.strGroupName, data)
 
   async def chat_message(self, data):
     data_json = {
       'message': data['message'],
+      'username': data['username'],
+      'datetime': data['datetime'],
     }
-
+    
     await self.send(text_data=json.dumps(data_json))
+
+  # 入室
+  async def join_chat(self):
+    self.strGroupName = 'chat'
+    await self.channel_layer.group_add(self.strGroupName, self.channel_name)
+
+  # 退室
+  async def leave_chat(self):
+    if('' == self.strGroupName):
+      return
+    
+    await self.channel_layer.group_discard(self.strGroupName, self.channel_name)
+
+    self.strGroupName = ''
--- room/urls.py
+++ room/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path( '', views.room, name='room' ),
+    path( 'room', views.room, name='room' ),
 ]
